5db8052dda424832175ccf54b0c743c844869766
rashil1582 <dogmonsterbot@gmail.com>
2024-10-20 20:57:19 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the navigation menu on mobile
+document.querySelector('.nav-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
